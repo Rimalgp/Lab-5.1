19710c3c008f2da3484e4c058b92fb839781621f
Gopal Rimal <rimalgp@miamioh.edu>
2025-05-13 14:35:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def index():
             {% else %}
                 <p>No game found.</p>
             {% endif %}
-            <a href="https://moomoo.io/?server=siliconvalley:VG" target="_blank">Play Moomooio</a>
+            <a href="https://www.chess.com/play/online:VG" target="_blank">Play Chess</a>
         </body>
         </html>
     ''', message=message, contacts=contacts)
